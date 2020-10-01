082b072d894c4bc727cca7cd3a05f23d10c206c6
niltondiniz <niltondiniz@niltondiniz.com>
2020-10-01 18:50:53 -0300

    feature/sharebutton_chave adicionado botao de compartilhar para compartilhar a chave gerada

--- lib/shared/util/consts.dart
+++ lib/shared/util/consts.dart
@@ -1,2 +1,4 @@
+// ignore: non_constant_identifier_names
 final String URL_ENDPOINT = "https://robust-sunbird-39.hasura.app/v1/graphql";
+// ignore: non_constant_identifier_names
 final String CHAVE_LICENCA = "250215";
